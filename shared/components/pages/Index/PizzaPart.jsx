4919c932a1a073703dac8c94f4bf51d5ff66b420
melamber <anton_shramkov@mail.ru>
2016-10-08 09:40:15 +0300

    small fixes

---
+++ shared/components/pages/Index/PizzaPart.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+
+export default class PizzaPart extends Component {
+
+    render() {
+
+        return (
+            <div className="topping-item-2 half-left"></div>
+        );
+    }
+}
